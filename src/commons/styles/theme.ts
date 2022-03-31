81cfbe2608695661c09f0d45c6e26e6986e90a92
Matheus Chignolli <matheuschignolli@gmail.com>
2022-03-30 23:31:58 -0300

    chore: add theme and material ui

--- src/commons/styles/theme.ts
+++ src/commons/styles/theme.ts
@@ -1,29 +1,43 @@
+import { createTheme } from '@mui/material/styles'
 import { spacing } from './utils'
 
-const colors = {
-  dark: {
-    primary: '#080334',
-    secondary: '#020D42',
-    tertiary: '#223167',
-    quaternary: '#05124B',
-    fifth: '#3E73D7',
-    white: '#ffffff',
+const dark = createTheme({
+  palette: {
+    primary: {
+      main: '#020d42',
+      light: '#35326e',
+      dark: '#00001d',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#223167',
+      light: '#515a95',
+      dark: '#000a3c',
+      contrastText: '#ffffff',
+    },
   },
-  light: {
-    primary: '#080334',
-    secondary: '#020D42',
-    tertiary: '#223167',
-    quaternary: '#05124B',
-    fifth: '#3E73D7',
-    white: '#ffffff',
-  },
-} as const
-
-const currentTheme = 'light'
-
-const theme = {
-  palette: colors[currentTheme],
   spacing,
-} as const
+})
 
-export default theme
+const light = createTheme({
+  palette: {
+    primary: {
+      main: '#64b5f6',
+      light: '#9be7ff',
+      dark: '#2286c3',
+      contrastText: '#000000',
+    },
+    secondary: {
+      main: '#4fc3f7',
+      light: '#8bf6ff',
+      dark: '#0093c4',
+      contrastText: '#000000',
+    },
+  },
+  spacing,
+})
+
+export default {
+  dark,
+  light,
+}

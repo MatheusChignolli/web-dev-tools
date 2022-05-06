e698fca5c44813bfdcf7235d7a99e9044c87ffc8
Matheus Chignolli <matheuschignolli@gmail.com>
2022-05-05 23:55:41 -0300

    add card component tests

---
+++ src/components/dumb/Card/__tests__/Card.test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import { render } from '~tests'
+import Card from '..'
+
+describe('Card', () => {
+  it('should render prop title and children', () => {
+    render(<Card title="Title">Content</Card>)
+
+    expect(screen.getByText('Title')).toBeInTheDocument()
+    expect(screen.getByText('Content')).toBeInTheDocument()
+  })
+
+  it('should not render prop title and not pass it', () => {
+    render(<Card>Content</Card>)
+
+    expect(screen.queryByLabelText('Título do card')).not.toBeInTheDocument()
+  })
+
+  it('should render keycaps for shortcuts', () => {
+    render(<Card keys={['Ctrl', 'Q']}>Content</Card>)
+
+    expect(screen.getByText('Ctrl')).toBeInTheDocument()
+    expect(screen.getByText('Q')).toBeInTheDocument()
+  })
+})

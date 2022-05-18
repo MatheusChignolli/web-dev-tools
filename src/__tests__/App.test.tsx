3cf65d066f25a54fa7c6d2cb95f811d088f90fb7
Matheus Chignolli <matheuschignolli@gmail.com>
2022-05-18 01:09:02 -0300

    Google Analytics

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,17 +1,6 @@
-import { ReactNode } from 'react'
 import { screen } from '@testing-library/react'
 import { render } from '~tests'
 import App from '../App'
-
-jest.mock('~providers', () => {
-  const originalModule = jest.requireActual('~providers')
-
-  return {
-    __esModule: true,
-    ...originalModule,
-    AnalyticsProvider: ({ children }: { children: ReactNode }) => children,
-  }
-})
 
 it('should render app', () => {
   render(<App />)

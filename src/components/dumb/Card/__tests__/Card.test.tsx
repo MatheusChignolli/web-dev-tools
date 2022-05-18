9d3e8d6f9ee371daa0c5dd5df0d3564eff2cb1bd
Matheus Chignolli <matheuschignolli@gmail.com>
2022-05-17 22:37:43 -0300

    .

--- src/components/dumb/Card/__tests__/Card.test.tsx
+++ src/components/dumb/Card/__tests__/Card.test.tsx
@@ -7,7 +7,7 @@ describe('Card', () => {
     render(<Card title="Title">Content</Card>)
 
     expect(screen.getByText('Title')).toBeInTheDocument()
-    expect(screen.getByText('asdasdasd')).toBeInTheDocument()
+    expect(screen.getByText('asdasdasdd')).toBeInTheDocument()
   })
 
   it('should not render prop title and not pass it', () => {

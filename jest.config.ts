78720f55bc215af640e812c74696e55144ff6410
Matheus Chignolli <matheuschignolli@gmail.com>
2022-05-17 22:08:03 -0300

    add tests

--- jest.config.ts
+++ jest.config.ts
@@ -17,6 +17,14 @@ const config: Config.InitialOptions = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setup-tests.ts'],
   moduleNameMapper: resolvedAliases,
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 }
 
 export default config

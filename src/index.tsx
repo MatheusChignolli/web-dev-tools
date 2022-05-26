5a7a0240dc989316ee9684d8898fa2ff5eedfb04
Matheus Chignolli <matheuschignolli@gmail.com>
2022-05-26 00:06:38 -0300

    remove unnecessary coments from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,11 @@ import '~i18n'
 import 'react-toastify/dist/ReactToastify.css'
 import reportWebVitals from './report-web-vitals'
 import App from './App'
+
 // TODO:
 // - Add aria-labels to many places
-// - Put toastfy for user feedback
-// - Add keyboard commands to generate CPF
 // - Add hamburguer menu to put the About page
 // - Create about page
-// - Responsive layout
-// - Create a name for the app
 // - Improve theme switch
 // - Add historic to methods
 // - add page for cpf, cnpj etc... regex
@@ -24,7 +21,4 @@ ReactDOM.render(
   document.querySelector('#root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()

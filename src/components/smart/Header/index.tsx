34c158e39cd2c5c3d24202af1b9f9b9b2361bd2a
Matheus Chignolli <matheuschignolli@gmail.com>
2023-01-22 19:44:01 -0300

    change theme button style

--- src/components/smart/Header/index.tsx
+++ src/components/smart/Header/index.tsx
@@ -1,8 +1,9 @@
-import { ChangeEvent, useMemo } from 'react'
 import { SelectChangeEvent } from '@mui/material'
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
 import { useRecoilState } from 'recoil'
 import { useTranslation } from 'react-i18next'
-import { Switch, Typography, Link } from '~components'
+import { Typography, Link, IconButton } from '~components'
 import { Languages } from '~interfaces/languages'
 import { languageState, themeState } from '~states'
 import { LanguagesConstants } from '~constants'
@@ -13,9 +14,8 @@ const Header = () => {
   const [theme, setTheme] = useRecoilState(themeState)
   const [language, setLanguage] = useRecoilState(languageState)
 
-  const handleThemeSwitch = (event: ChangeEvent<HTMLInputElement>) => {
-    const checked = event.target.checked
-    setTheme(checked ? 'dark' : 'light')
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
   }
 
   const handleLanguageSelect = (event: SelectChangeEvent<unknown>) => {
@@ -23,20 +23,15 @@ const Header = () => {
     setLanguage(selectedLanguage)
   }
 
-  const switchChecked = useMemo(() => theme === 'dark', [theme])
-
   return (
     <HeaderStyled aria-label={t<string>('header.ariaLabels.section')}>
       <Link.Internal to="/">
         <Typography variant="h5">{t<string>('header.title')}</Typography>
       </Link.Internal>
       <div>
-        <Switch
-          onChange={handleThemeSwitch}
-          color="default"
-          checked={switchChecked}
-          aria-label={t<string>('header.ariaLabels.themeSwitch')}
-        />
+        <IconButton sx={{ p: 1, mr: 2 }} onClick={handleThemeSwitch} color="inherit">
+          {theme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
         <SelectStyled
           variant="filled"
           onChange={handleLanguageSelect}
